ac28c32643adece1939bd2ecefdcf437412c827f
madisonnolen <madisonnolen03@gmail.com>
2025-07-24 20:18:30 -0700

    End of video 6: Database and output

--- app/src/main/java/com/example/hw03gymlog/MainActivity.java
+++ app/src/main/java/com/example/hw03gymlog/MainActivity.java
@@ -13,6 +13,7 @@ import com.example.hw03gymlog.database.GymLogRepository;
 import com.example.hw03gymlog.database.entities.GymLog;
 import com.example.hw03gymlog.databinding.ActivityMainBinding;
 
+import java.util.ArrayList;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
@@ -36,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
         repository = GymLogRepository.getRepository(getApplication());
 
         binding.logDisplayTextView.setMovementMethod(new ScrollingMovementMethod());
+        updateDisplay();
 
         binding.logButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,15 +52,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void insertGymLogRecord() {
+        if(mExercise.isEmpty()){
+            return;
+        }
         GymLog log = new GymLog(mExercise, mWeight, mReps);
         repository.insertGymLog(log);
     }
     private void updateDisplay(){
-        String currentInfo = binding.logDisplayTextView.getText().toString();
-        Log.d(TAG, "Current info: " + currentInfo);
-        String newDisplay = String.format(Locale.US,"Exercise:%s%nWeight:%.2f%nReps:%d%n=-=-=-=%n%s", mExercise, mWeight,mReps, currentInfo);
-        binding.logDisplayTextView.setText(newDisplay);
-        Log.i(TAG, repository.getAllLogs().toString());
+        ArrayList<GymLog> allLogs = repository.getAllLogs();
+        if(allLogs.isEmpty()){
+            binding.logDisplayTextView.setText(R.string.nothing_to_show_time_to_hit_the_gym);
+        }
+        StringBuilder sb = new StringBuilder();
+        for(GymLog log : allLogs){
+            sb.append(log);
+        }
+
+        binding.logDisplayTextView.setText(sb.toString());
     }
 
     private void getInformationFromDisplay(){
